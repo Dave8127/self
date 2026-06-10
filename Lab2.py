b5739895fce9498a45046e623f105023c3cf3cdd
Dave8127 <nemishadave2007@gmail.com>
2026-06-10 12:21:28 +0530

    File upload

---
+++ Lab2.py
@@ -0,0 +1,35 @@
+a = float(input("Enter Number : "))
+b = float(input("Enter Number : "))
+
+print(a)
+print(b)
+
+print("Addition")
+
+c = a + b
+print("Addition of",a,"and",b,"is",c)
+
+print("substraction")
+
+c = a - b
+print("substraction of",a,"and",b,"is",c)
+
+print("multiplication")
+
+c = a * b
+print("multiplication of",a,"and",b,"is",c)
+
+print("division")
+
+c = a // b
+print("division of",a,"and",b,"is",c)
+
+print("Modules")
+
+c = a % b
+print("modules of",a,"and",b,"is",c)
+
+print("Expontiation")
+
+c = a ** b
+print("Expontiation of",a,"and",b,"is",c)
