b2f979175a801a42039dfe7920ae2af4d290b103
Dave8127 <nemishadave2007@gmail.com>
2026-06-10 11:22:49 +0530

    First commit

---
+++ trial.py
@@ -0,0 +1,8 @@
+print("Trial project")
+
+name = str(input("Enter Name : "))
+age  = int(input("Enter age  : "))
+hobby = str(input("Enter Your Hobby : "))
+
+print("Hellow ! My Name Is ",name,"and i m Enjoying My Hobby ",hobby,"At the age Of ",age)
+
